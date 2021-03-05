c315b50f3cd01b5bc8cd054bb92ef9f66d5d495f
Taichir0923 <crystal19950923@gmail.com>
2021-03-05 14:17:08 +0800

    vcxvx

--- script.js
+++ script.js
@@ -1,21 +1,44 @@
-var list = document.querySelector('#list');
-var input = document.querySelector('#insertTask');
+let list = document.querySelector('#list');
+let input = document.querySelector('#insertTask');
 
 
 function insertTask(){
     if(!input.value) return false; // input.value.length, input.value == ''
     list.innerHTML += '<li>' + '<p>' + input.value + '</p>' + '</li>';
     input.value = '';
-    var lists = document.querySelectorAll('li');
+    let lists = document.querySelectorAll('#list li');
+
     lists.forEach(e => {
-        if(e.childNodes.length < 2) {
-            e.insertAdjacentHTML('beforeend', '<button>&times</button>');
-        }
         e.onclick = () => {
             e.childNodes[0].classList.toggle('done');
         }
+
+        if(e.childNodes.length < 2){
+            e.insertAdjacentHTML('beforeend' , '<button>&times</button>');
+        }
+
         e.childNodes[1].onclick = () => {
             e.remove();
         }
     })
 }
+
+document.addEventListener('keydown', (event) => {
+    if(event.keyCode === 13){
+        insertTask();
+    }
+})
+
+
+
+// ES6 -> forEach => Loop;
+
+// loop, insertAdjacentHTML, remove(), event
+
+// let headingTag = document.querySelector('#testHTML');
+// headingTag.onclick = () => {
+//     headingTag.insertAdjacentHTML('afterend', '<p>hi</p>');
+// }
+
+// arr.pop
+
